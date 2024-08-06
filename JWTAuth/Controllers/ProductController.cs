52a8de75974eefaeb844b51092d258ea952391b4
Ullas.HU <ullas.hu@ust.com>
2024-08-06 13:02:32 +0530

    Product controller added.

---
+++ JWTAuth/Controllers/ProductController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JWTAuth.Controllers
+{
+    [Authorize]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetProduct()
+        {
+            return Ok("Enterd product repo!!!");
+        }
+    }
+}
