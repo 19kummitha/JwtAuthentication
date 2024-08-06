9dc5e42f470eb437644b7608154b33c9219e8932
VenkataKotiReddy.Kummitha@ust.com <VenkataKotiReddy.Kummitha@ust.com>
2024-08-06 12:27:28 +0530

    Migration is done and databse is updated in my machine.

--- JWTAuth/Program.cs
+++ JWTAuth/Program.cs
@@ -1,8 +1,16 @@
+using JWTAuth.Context;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddDbContext<AuthenticationDbContext>(options =>
+{
+    var Connectionstring = builder.Configuration.GetConnectionString("AuthDbConnection");
+    options.UseSqlServer(Connectionstring);
+});
 
 var app = builder.Build();
 
